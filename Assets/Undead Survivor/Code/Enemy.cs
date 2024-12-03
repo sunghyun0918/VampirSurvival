f209d1bace640725cf4a70744e880bbbe4de473f
sunghyun.lee <sunghyun.lee@elixiratelier.com>
2024-12-04 02:04:33 +0900

    6. 오브젝트 풀링으로 소환하기

--- Assets/Undead Survivor/Code/Enemy.cs
+++ Assets/Undead Survivor/Code/Enemy.cs
@@ -35,4 +35,8 @@ public class Enemy : MonoBehaviour
             return;
         spriter.flipX = target.position.x < rigid.position.x;
     }
+    void OnEnable()
+    {
+        target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+    }
 }
