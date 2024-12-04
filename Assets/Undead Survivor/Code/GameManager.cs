35c2fd34e6f7e8ec1bf19355a710acc80a237ad3
sunghyun.lee <sunghyun.lee@elixiratelier.com>
2024-12-05 07:49:53 +0900

    13. 게임 시작과 종료

--- Assets/Undead Survivor/Code/GameManager.cs
+++ Assets/Undead Survivor/Code/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -10,8 +12,8 @@ public class GameManager : MonoBehaviour
     public float gameTime;
     public float maxGameTime = 2 * 10f;
     [Header("# player Info")]
-    public int health;
-    public int maxHealth = 100;
+    public float health;
+    public float maxHealth = 100;
     public int level;
     public int kill;
     public int exp;
@@ -21,17 +23,50 @@ public class GameManager : MonoBehaviour
     public PoolManager pool;
     public Player player;
     public LevelUp uiLevelUp;
-
+    public Result uiResult;
+    public GameObject enemyCleaner;
     void Awake()
     { 
         instance = this;
     }
-    void Start()
+    public void GameStart()
     {
         health = maxHealth;
+        uiLevelUp.Select(0); // 임시 스크립트 (첫 번째 캐릭터 선택)
+        Resume();
+    }
+    public void GameOver()
+    {
+        StartCoroutine(GameOverRoutine());
+    }
 
-        // 임시 스크립트 (첫 번째 캐릭터 선택)
-        uiLevelUp.Select(0);
+    IEnumerator GameOverRoutine()
+    {
+        isLive = false;
+        yield return new WaitForSeconds(0.5f);
+        uiResult.gameObject.SetActive(true);
+        uiResult.Lose();
+        Stop();
+    }
+    public void GameVictroy()
+    {
+        StartCoroutine(GameVictroyRoutine());
+    }
+
+    IEnumerator GameVictroyRoutine()
+    {
+        isLive = false;
+        enemyCleaner.SetActive(true);
+
+        yield return new WaitForSeconds(0.5f);
+
+        uiResult.gameObject.SetActive(true);
+        uiResult.Win();
+        Stop();
+    }
+    public void GameRetry()
+    {
+        SceneManager.LoadScene(0);
     }
     void Update()
     {
@@ -41,10 +76,14 @@ public class GameManager : MonoBehaviour
         if(gameTime > maxGameTime)
         {
             gameTime = maxGameTime;
+            GameVictroy();
         }
     }
     public void Getexp()
     {
+        if(!isLive)
+            return;
+
         exp++;
         if(exp == nextExp[Mathf.Min(level, nextExp.Length-1)])
         {
